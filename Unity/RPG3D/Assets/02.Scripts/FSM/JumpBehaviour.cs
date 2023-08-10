d4b11d64a6565cb4328e5d81c56a4f85db6c815e
KoTaeseong <eve3779@gmail.com>
2023-08-10 19:55:56 +0900

    230810 RPG3D MachineBehaviour, Jump,Fall,Land,Somersault

---
+++ Unity/RPG3D/Assets/02.Scripts/FSM/JumpBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace RPG.FSM
+{
+    public class JumpBehaviour : BehaviourBase
+    {
+        [SerializeField] float _jumpForce = 5.0f;
+
+        public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+        {
+            base.OnStateMachineEnter(animator, stateMachinePathHash);
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x,
+                                            0.0f,
+                                            rigidbody.velocity.z);
+            rigidbody.AddForce(Vector3.up * _jumpForce,ForceMode.Impulse);
+            manager.hasJumped = true;
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+            if(rigidbody.velocity.y <= 0)
+            {
+                manager.ChagneState(animator, StateType.Fall);
+            }
+        }
+    }
+}
+
+
+
+
